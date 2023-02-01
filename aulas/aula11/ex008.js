55813b06be7283e2f348bb5463c6e65ce3e78e5c
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-01 19:09:31 -0300

    Condições em  JavaScript

---
+++ aulas/aula11/ex008.js
@@ -0,0 +1,10 @@
+var vel = 82
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+
+if (vel > 60){
+    console.log(`Você foi multado por estar à cima do limite de velocidade. O radar contou ${vel} Km/h`)
+    console.log('Pague a sua multa o quanto antes!')
+} else {
+    console.log(`Sua velocidade de ${vel} Km/h está dentro do limite.`)
+    console.log('Dirija sempre usando sinto de segurança')
+}
---
+++ aulas/aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Você está vivendo em ${pais} atualmente.`)
+if (pais == 'Brasil'){
+    console.log('Portanto, você é brasileiro.')
+} else {
+    console.log('Portanto, você é um estrangeiro.')
+}
