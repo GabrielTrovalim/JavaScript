d2559abe1a1d3d283d47ffcf351c9e741b35d8da
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-03 16:33:42 -0300

    Update script01.js

--- Exercícios/d002/script01.js
+++ Exercícios/d002/script01.js
@@ -2,7 +2,7 @@ function calcular(){
     var time = new Date()
     var atano = time.getFullYear()
     var txtano = window.document.getElementById('txtnun')
-    var resu = window.document.getElementById('resultado')
+    var resu = window.document.getElementById('resultado')    
     var nano = Number(txtano.value)
     var soma = atano - nano
     
@@ -10,6 +10,10 @@ function calcular(){
         window.alert('Invalido')
     } else if (soma > 100) {
         resu.innerHTML = `Você provavelmente já morreu, ou tem ${soma} anos...`
+    } else if (soma == 0) {
+        resu.innerHTML = `Você é um recém nascido!`
+    } else if (soma == 1) {
+        resu.innerHTML = `Você tem apenas ${soma} ano!`
     } else {
         resu.innerHTML = `Você tem ${soma} anos!`
     }
