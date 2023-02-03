2cbec1a6ef6dd5d0d5125494975ca660cff21875
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-02 22:29:24 -0300

    mudando a imagem conforme o tempo

--- Exercícios/modelo/scipt.js
+++ Exercícios/modelo/scipt.js
@@ -1,8 +1,15 @@
 function carrega(){
     var texto = window.document.getElementById('msg')
-    var foto = window.document.getElementById('imagem')
+    var foto = window.document.getElementById('foto')
     var hora = new Date()
-    var time = hora.getHours()
-    var mins = hora.getMinutes()
+    var time = hora.getHours() // Pegando as horas pelo OS.
+    var mins = hora.getMinutes() // Pegando os mintos pelo OS.
     texto.innerHTML = `Agora são ${time} horas e ${mins} minutos.`
+    if (time >= 6 && time < 12){
+        foto.src = 'imagens/manhã.jpg'
+    } else if (time >= 12 && time < 18){
+        foto.src = 'imagens/tarde.jpg'
+    } else {
+        foto.src = 'imagens/noite.jpg'
+    }
 }
