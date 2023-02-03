89a42d8ff5327605f0498ed49cef654f142f85d9
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-02 22:53:36 -0300

    toques finais

--- Exercícios/modelo/scipt.js
+++ Exercícios/modelo/scipt.js
@@ -2,14 +2,18 @@ function carrega(){
     var texto = window.document.getElementById('msg')
     var foto = window.document.getElementById('foto')
     var hora = new Date()
-    var time = hora.getHours() // Pegando as horas pelo OS.
+    /*var time = hora.getHours() // Pegando as horas pelo OS.*/
+    var time = 8
     var mins = hora.getMinutes() // Pegando os mintos pelo OS.
     texto.innerHTML = `Agora são ${time} horas e ${mins} minutos.`
-    if (time >= 6 && time < 12){
+    if (time >= 6 && time <= 12){
         foto.src = 'imagens/manhã.jpg'
-    } else if (time >= 12 && time < 18){
+        document.body.style.background = '#cdd377'
+    } else if (time >= 12 && time <= 18){
         foto.src = 'imagens/tarde.jpg'
+        document.body.style.background = '#fca558'
     } else {
         foto.src = 'imagens/noite.jpg'
+        document.body.style.background = '#17242c'
     }
 }
