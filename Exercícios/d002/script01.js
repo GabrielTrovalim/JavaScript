3b6668985e448840f3e5a2bda844d477fff10aeb
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-06 16:29:43 -0300

    configurando script
    Agora o script identifica a diferença entre homens e mulheres além da idade informada.

--- Exercícios/d002/script01.js
+++ Exercícios/d002/script01.js
@@ -2,10 +2,39 @@ function calcular(){
     var time = new Date()
     var atano = time.getFullYear()
     var txtano = window.document.getElementById('txtnun')
-    var resu = window.document.getElementById('resultado')    
+    var resu = window.document.getElementById('resultado')   
+
      if (txtano.value.length == 0 || txtano.value > atano ){
-        window.alert('Entrada de dados invalida!')
-     } else{ 
-        window.alert('tá de boa!')
+        window.alert('[ERRO!] Entrada de dados invalida!')
+     } else { // dando a permissão para o código prosseguir.
+        var sex = document.getElementsByName('botsex')
+        var idade = atano - Number(txtano.value)
+        var gen = ``
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if(sex[0].checked) {
+            gen = `um Homem`
+            if (idade >= 0  && idade <= 10){
+                //criança
+            } else if (idade < 21) {
+                // jovem
+            } else if (idade < 50) {
+                // adulto
+            } else {
+                //idoso
+            }
+        } else if (sex[1].checked) {
+            gen = `uma Mulher`
+            if (idade >= 0 && idade <= 10){
+                //criança
+            } else if (idade < 21) {
+                // jovem
+            } else if (idade < 50) {
+                // adulto
+            } else {
+                // idoso
+            }
+        }
      }
+     resu.innerHTML = `Detectamos ${gen} com ${idade} anos!`
 }
