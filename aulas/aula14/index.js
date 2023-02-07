88e9364692526553d0a636283677479bfd3b4a41
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-06 23:21:55 -0300

    teste de laço for

---
+++ aulas/aula14/index.js
@@ -0,0 +1,16 @@
+/*
+var c = 1 
+
+while (c <= 5){
+    console.log(c)
+    c ++
+} // Estrutura de repetição while (Enquato...)
+*/
+
+// Agora em for()
+
+console.log('vai começar...')
+for(c = 1; c <= 5; c ++){
+    console.log('carambolas...')
+} // é a mesma coisa dos outros laços, só que mais simples e menor.
+console.log('fim')
