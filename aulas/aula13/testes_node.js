392741e3c126416920de5db43ae648775a7fce46
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-06 22:38:48 -0300

    testando laços

--- aulas/aula13/testes_node.js
+++ aulas/aula13/testes_node.js
@@ -1,1 +1,15 @@
-console.log('olá, mundoooo!')
+var c = 1
+
+while (c <= 6) {
+    console.log('olá!')
+    c ++
+}
+
+// outra estrutura de repetição:
+
+var cont = 1
+
+do{
+    console.log('Hello!')
+    cont ++
+} while (cont <= 10)
