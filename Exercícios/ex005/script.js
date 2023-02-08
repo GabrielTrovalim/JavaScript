f1219fcc8b3fe66fc924d55c3849c68babc9d43a
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-07 22:38:43 -0300

    Atualização de funcionalidade
    Agora ao gerar uma nova tabuada a antiga é apagada, antes que o laço possa ser executado.

--- Exercícios/ex005/script.js
+++ Exercícios/ex005/script.js
@@ -7,7 +7,7 @@ function gerar(){
     if (txtnum.value.length == 0){ // Verificação de entrada de dados.
         window.alert('[Erro] Entrada de dados incompleta')
     } else {
-        
+        res.innerHTML = ``
         for(var c = 1; c <= 10; c++){
             var tabu = (num * c) // Multiplicando uma variável pelo contador.
             res.innerHTML += `<br> ${num} X ${c} = ${tabu} <br>`
@@ -16,6 +16,6 @@ function gerar(){
 }
 function limpar(){
     var res = window.document.querySelector('div#resu')
-
+    // Criando uma função que limpar tanto a entrada quanto a saída de dados.
     res.innerHTML = `Resultado...`
 }
