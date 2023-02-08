af1dda4f61a44680549ba8bcbd35aa4128dfc329
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-07 21:17:02 -0300

    Atualizações

--- Exercícios/ex004/script.js
+++ Exercícios/ex004/script.js
@@ -10,14 +10,27 @@ function contar(){
 
     var res = window.document.getElementById('resu')
 
-    var c = n1
-    while (c <= n2){
-        res.innerHTML = `${c}`
-        c *= n3
+    if (start.value.length == 0 || end.value.length == 0 || step.value.length == 0){
+        window.alert('[Erro] Entrada de dados invalida')
+    } else {
+        if (n3 <= 0){
+            window.alert('Valor invalido. Considerando Passo = 1')
+            n3 = 1
+        }
+        if (n1 < n2) { // Contagem crescente.
+            for (var c = n1; c <= n2; c += n3) {
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        } else { // Contagem regressiva.
+            for (var c = n1; c >= n2; c -= n3) {
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        }
+        res.innerHTML += ` \u{1F3C1}`
     }
 }
 function limpar(){
     var res = window.document.getElementById('resu')
 
-    res.innerHTML = `Aguardando resultado...`
+    res.innerHTML = ``
 }
