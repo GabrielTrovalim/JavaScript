75044cb4f17b2c812879d2cb154f6fe732bb1f6f
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-11 07:30:36 -0300

    função 02
    Esse exemplo de função de JS mostra que é possível atribuir um valor prévio para um parâmetro de função, evitando possíveis erros.

---
+++ aulas/aula16/func02.js
@@ -0,0 +1,6 @@
+function soma (n1 = 0, n2 = 0) { // Definindo valores prévios para os parâmetros.
+    return n1 + n2 // return sem a necessidade de definir number ou string.
+}
+
+var opa = soma(10, 2) // Chamada que pode ou não ter os dois valores.
+console.log(opa) // Output.
